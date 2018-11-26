d102f424bcee01eca92e2fb1ea291ff7c076e14b
alphiek <43937504+alphiek@users.noreply.github.com>
2018-11-26 18:01:25 +0200

    Update to businesslist
    Update to businesslist

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,12 +6,10 @@ class BusinessList extends React.Component {
   render () {
     return (
     <div className="BusinessList">
-    {
-      this.props.businesses.map(business => {
-        return <Business business={this.business}
+    {this.props.businesses.map(business => {
+        return <Business business={business}
         key={business.id} />
-      })
-    }
+      })}
     </div>
   );
 }
